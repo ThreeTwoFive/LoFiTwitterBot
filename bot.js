40c1538b1cf7955a95b309267731b67a55d70140
NormalHero <dtruong510805@gmail.com>
2017-07-02 18:14:51 -0700

    Creating functions

--- bot.js
+++ bot.js
@@ -1,30 +1,48 @@
 var Twit = require('twit');
 var configKeys = require('./config.js');
 
-var bot = new Twit(configKeys);
+function postDailySongTweet(){
+    var bot = new Twit(configKeys);
+    
+    var links = ['https://www.youtube.com/watch?v=6mtn1YWyJas',
+                'https://www.youtube.com/watch?v=DhHGDOgjie4',
+                'https://www.youtube.com/watch?v=7D4kEdnShrs',
+                'https://www.youtube.com/watch?v=dIC4VSUE7q4',
+                'https://www.youtube.com/watch?v=HbGoW-L_zwk',
+                'https://www.youtube.com/watch?v=fOy1esPEc08',
+                'https://www.youtube.com/watch?v=UvO6PuPFpYc',
+                'https://www.youtube.com/watch?v=nLEOwgm7OUQ',
+                'https://www.youtube.com/watch?v=TGLQ5C8JYZw',
+                'https://www.youtube.com/watch?v=jo4-FhqkNwQ',
+                'https://www.youtube.com/watch?v=fgGFNZWEIcU',
+                'https://www.youtube.com/watch?v=HYLxs7Gonac'];
+    
+    getDailySongs();
 
-//12 links
-var links = ['https://www.youtube.com/watch?v=6mtn1YWyJas',
-             'https://www.youtube.com/watch?v=DhHGDOgjie4',
-             'https://www.youtube.com/watch?v=7D4kEdnShrs',
-             'https://www.youtube.com/watch?v=dIC4VSUE7q4',
-             'https://www.youtube.com/watch?v=HbGoW-L_zwk',
-             'https://www.youtube.com/watch?v=fOy1esPEc08',
-             'https://www.youtube.com/watch?v=UvO6PuPFpYc',
-             'https://www.youtube.com/watch?v=nLEOwgm7OUQ',
-             'https://www.youtube.com/watch?v=TGLQ5C8JYZw',
-             'https://www.youtube.com/watch?v=jo4-FhqkNwQ',
-             'https://www.youtube.com/watch?v=fgGFNZWEIcU',
-             'https://www.youtube.com/watch?v=HYLxs7Gonac'];
-//test post
-bot.post('statuses/update', 
-    {status: 'New Hour New Vibes '
-        + links[Math.floor(Math.random() * links.length)]
-    }, 
-    function(err, data, response){
+    bot.post('statuses/update', 
+        {status: 'New Hour New Vibes '
+            + links[Math.floor(Math.random() * links.length)]
+        }, 
+        function(err, data, response){
 
-    console.log(data);
-})
+        console.log(data);
+    })
+
+}
+
+function getDailySongs(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", 'url');
+    xhr.send();
+
+    console.log(JSON.parse(xhr.responseText));
+}
+
+
+/*
+    Move other functions to another file
+    They need to be run at different intervals or constantly
+*/
 
 //test post status on mention
 /*
@@ -34,9 +52,7 @@ bot.get('statuses/mentions_timeline', function(err, data, response){
 */
 //test post at certain time (Daily post feature)
 
-
 //Need function to scrape new song to post or grab from resource file
-
 
 //Retweet tweets with hashtag #lofi #lofihiphop etc
 
